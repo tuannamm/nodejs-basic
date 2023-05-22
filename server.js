e64c793f51209b2f09f1f08bf3429d6622bb77f7
Tuan Nam <nttnnam@gmail.com>
2023-05-22 15:14:58 +0700

    create route

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const express = require("express");
-// import express from "express";
-const app = express();
-const port = 3000;
+const express = require("express"); // commonjs
+// import express from "express"; // es modules
 
+const app = express(); // app express
+const port = 8080; // port
+
+// khai bao route
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/test", (req, res) => {
+  res.send("Hello Tuan Nam");
+});
+
+app.get("/tuannam", (req, res) => {
+  res.send("<h1>Tuan Nam is the best</h1>");
+});
+
 app.listen(port, () => {
-  console.log(`App is running at port ${port}`);
+  console.log(`App is running at port ${8080}`);
 });
