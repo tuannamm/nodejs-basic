c8f3ea4f6e755719b4c8a722b2b7b07310ff94ff
Tuan Nam <nttnnam@gmail.com>
2023-05-22 14:50:06 +0700

    hello world with expressjs

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const http = require("http");
+const express = require("express");
+// import express from "express";
+const app = express();
+const port = 3000;
 
-const hostname = "localhost";
-const port = 2023;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\nTuan Nam is the best");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`App is running at port ${port}`);
 });
