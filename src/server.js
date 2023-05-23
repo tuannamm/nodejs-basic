5497277dec4aab1b6789d30878db3f44df099e04
Tuan Nam <nttnnam@gmail.com>
2023-05-23 21:04:33 +0700

    create connection in database.js file

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const express = require("express"); // commonjs
-const path = require("path");
-const mysql = require("mysql2");
 require("dotenv").config(); // load bien moi truong
 
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const connection = require("./config/database");
 
 const app = express(); // app express
 const port = process.env.PORT || 8080; // port
@@ -16,20 +15,10 @@ configViewEngine(app);
 // khai bao route
 app.use("/", webRoutes);
 
-// test connection
-// create the connection to database
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307, // default 3306
-  user: "root",
-  password: "123456",
-  database: "hoidanit",
-});
-
 // simple query
 connection.query("SELECT * FROM Users", function (err, results, fields) {
-  console.log("results", results); // results contains rows returned by server
-  console.log("fields", fields); // fields contains extra meta data about results, if available
+  console.log("results", results);
+  console.log("fields", fields);
 });
 
 app.listen(port, hostname, () => {
