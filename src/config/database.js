6763056a917b170e1b99e3d2068a64649811347c
Tuan Nam <nttnnam@gmail.com>
2023-05-25 15:16:27 +0700

    create connection pool

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,23 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 // create the connection to database
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT, // default 3306
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT, // default 3306
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = connection;
