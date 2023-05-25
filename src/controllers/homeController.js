15160f89ab56dd6fa9c6afac7e585e15bff9da92
Tuan Nam <nttnnam@gmail.com>
2023-05-25 19:10:30 +0700

    save data user to database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,5 @@
+const connection = require("../config/database");
+
 const getHomepage = (req, res) => {
   return res.render("home.ejs");
 };
@@ -11,8 +13,15 @@ const getTuanNam = (req, res) => {
 };
 
 const postCreateUser = (req, res) => {
-  console.log("req.body", req.body);
-  res.send("Hello Tuan Nammmmm");
+  let { email, name, city } = req.body;
+  connection.query(
+    `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
+    [email, name, city],
+    function (err, results) {
+      console.log("results", results);
+      res.send("Create user successfully");
+    }
+  );
 };
 
 module.exports = {
