c8385413445270a09088c558746d46fb61d60bf5
Tuan Nam <nttnnam@gmail.com>
2024-01-02 15:00:36 +0700

    59-create-database

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+// mặc định sẽ chỉ xuống database được đặt tên là test nếu không chỉ đỉnh kết nối database nào
+
 const connection = async() => {
   try {
     const options = {
       user: process.env.DB_USER,
       pass: process.env.DB_PASSWORD,
-
+      dbName: process.env.DB_NAME
     };
     console.log("connecting database");
     await mongoose.connect(process.env.DB_HOST, options);
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ require("dotenv").config(); // load bien moi truong
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const connection = require("./config/database");
+const { default: mongoose } = require("mongoose");
 
 const app = express(); // app express
 const port = process.env.PORT || 8080; // port
@@ -18,6 +19,15 @@ configViewEngine(app);
 
 // khai bao route
 app.use("/", webRoutes);
+
+const kittySchema = new mongoose.Schema({
+  name: String
+})
+
+const Kitten = mongoose.model("Kitten", kittySchema);
+const cat = new Kitten({name: "Tuan Nam's cat"});
+
+cat.save();
 
 (async () => {
   try {
